b9e8a5d608771e1416f1d34a44ded82a444d63ae
Jay Mehta <jay.m@yudiz.com>
2022-02-25 10:15:51 +0530

    Added Scripts
    - Spawn Objects

---
+++ Assets/GameItem.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public enum ObjectType
+{
+    Touchable = 0,
+    Nontouchable = 1
+}
+
+public class GameItem : MonoBehaviour
+{
+   
+   [SerializeField] ObjectType itemType;
+   [SerializeField] float  objectSeconds=0;
+   [SerializeField] Image processImage = null;
+   [SerializeField] ParticleSystem destroyEffect = null; 
+   [SerializeField] int positiveScore = 0;
+   [SerializeField] int negativeScore = 0;
+   [SerializeField] TextMeshProUGUI animText;
+
+    
+    
+    public ObjectType ItemType
+    {
+       get{return this.itemType;}
+       set{this.itemType =value;}
+    }
+
+
+
+    public Transform GetTransform()
+    {return this.transform;}
+    
+    private void Destroy() {
+            
+                Destroy(this.gameObject);
+         
+         }
+
+
+
+}
---
+++ Assets/SpawnItems.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnItems : MonoBehaviour
+{
+
+   private  float screenHalfWidthInWold;
+    private float screenHalfHeightInWold;
+   [SerializeField] private GameItem item;
+   [SerializeField] private int generateTimeing = 3;
+   [SerializeField] private int numberOfGenerateAtTime = 3;
+   
+    void Start()
+    {
+        screenHalfHeightInWold = (Camera.main.orthographicSize - (item.transform.localScale.x / 2));
+        screenHalfWidthInWold = (Camera.main.aspect * Camera.main.orthographicSize) - (item.transform.localScale.x / 2);
+
+        for(int i=1;i<10;i++)
+        {
+            GenerateItem(ObjectType.Touchable);
+        }
+    }
+
+
+    public void GenerateItem(ObjectType type)
+    {
+        var x = Random.RandomRange(-screenHalfWidthInWold,screenHalfWidthInWold);
+        var y = Random.RandomRange(-screenHalfHeightInWold,screenHalfHeightInWold);
+        Vector3 location = new Vector3(x,y,0f);
+        item.ItemType = type;
+        
+       var obj =  Instantiate(item.gameObject,location,Quaternion.identity,this.transform);
+       var oPos =  obj.transform.localPosition;
+      obj.transform.localPosition = new Vector3(oPos.x,oPos.y,0f); 
+
+    }
+
+   
+}
